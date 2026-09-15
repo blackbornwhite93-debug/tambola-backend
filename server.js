b0842afa4084fd4f59938135e5758b5e6ed5ccbe
YOUR_GITHUB_USERNAME <YOUR_EMAIL@example.com>
2026-09-15 20:46:05 +0530

    Integrated custom timer and Full House logic stack

--- server.js
+++ server.js
@@ -12,22 +12,26 @@ let gameInterval = null;
 function drawNumber() {
     if (availableNumbers.length === 0) {
         clearInterval(gameInterval);
+        gameInterval = null;
         io.emit('game-over', { message: 'All numbers drawn!' });
         return;
     }
     const randomIndex = Math.floor(Math.random() * availableNumbers.length);
     const num = availableNumbers.splice(randomIndex, 1)[0];
     drawnNumbers.push(num);
-    
     io.emit('number-drawn', { current: num, history: drawnNumbers });
 }
 
 io.on('connection', (socket) => {
     socket.emit('sync-state', { history: drawnNumbers });
 
-    socket.on('start-game', () => {
+    socket.on('start-game', (data) => {
         if (!gameInterval) {
-            gameInterval = setInterval(drawNumber, 5000);
+            const speed = data && data.speed ? data.speed : 5000;
+            // Wipes any stale old games automatically on restart
+            availableNumbers = Array.from({length: 90}, (_, i) => i + 1);
+            drawnNumbers = [];
+            gameInterval = setInterval(drawNumber, speed);
         }
     });
 });
